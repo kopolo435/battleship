0878c6f68dacee46c14f965e2b7cafcb0a2efd1c
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-23 17:51:16 -0500

    Implementa funciones basicas de controlador de juego

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import hello from "./prueba";
+import Player from "./player";
+import extractCoordinates from "./extractCoordinates";
+
+function createPlayer(nameElement, isComputerElement) {
+  const isComputer = isComputerElement.value === "true";
+  return new Player(nameElement.value, isComputer);
+}
+
+function turnLoops(initialPlayer, secondPlayer, chooseAttack) {
+  let currentPlayer = initialPlayer;
+  let enemy = secondPlayer;
+  while (!enemy.getGameboard().allShipsSunk()) {
+    let attack;
+    if (currentPlayer.getIsComputer()) {
+      attack = currentPlayer.getComputerPlay(enemy.getGameboard().getCells());
+    } else {
+      attack = chooseAttack();
+    }
+    enemy.getGameboard().receiveAttack(attack);
+    if (enemy.getGameboard().allShipsSunk()) {
+      return currentPlayer.getName();
+    }
+    const temp = currentPlayer;
+    currentPlayer = enemy;
+    enemy = temp;
+  }
+  return null;
+}
+
+export { createPlayer, turnLoops };
