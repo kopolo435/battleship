fbc59c1e9b51d79ea70a09efcf0b2b2aa3c2f0bc
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-23 17:50:14 -0500

    Agrega test para controlador de juego

---
+++ tests/mainLoop.test.js
@@ -0,0 +1,29 @@
+import * as mainLoop from "../src/index";
+import Player from "../src/player";
+
+test("Player creation", () => {
+  const mockGetName = jest.fn(() => ({ value: "samir" }));
+  const mockGetIsComputer = jest.fn(() => ({ value: "false" }));
+  expect(
+    mainLoop.createPlayer(mockGetName(), mockGetIsComputer())
+  ).toStrictEqual(new Player("samir", false));
+});
+
+test("Turn loop with computer, human win", () => {
+  const player1 = new Player("samir", false);
+  player1.getGameboard().addShip("[0,0]", 1, "horizontal");
+  const player2 = new Player("computer", true);
+  player2.getGameboard().addShip("[3,4]", 1, "horizontal");
+  const mockAttack = jest.fn();
+  mockAttack.mockReturnValueOnce("[3,4]");
+  expect(mainLoop.turnLoops(player2, player1, mockAttack)).toBe("samir");
+});
+
+test("Turn loop with computer, computer win", () => {
+  const player1 = new Player("samir", false);
+  player1.getGameboard().addShip("[0,0]", 3, "horizontal");
+  const player2 = new Player("computer", true);
+  player2.getGameboard().addShip("[3,4]", 2, "horizontal");
+  const mockAttack = jest.fn(() => "[3,4]");
+  expect(mainLoop.turnLoops(player2, player1, mockAttack)).toBe("computer");
+});
