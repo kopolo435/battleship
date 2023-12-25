566bcca8f0b987430d7a61d90984d3153cb36b2c
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-24 21:07:31 -0500

    Agrega funcionalidad de movimiento entre paginas setup
    Agrega lo necesario para que los usuarios puedan moverse entre los
    setups y una vez terminados, moverse al board principal

--- src/shipSetup.js
+++ src/shipSetup.js
@@ -1,3 +1,32 @@
 import style from "./shipSetup.css";
 
+const readyBtn = document.getElementById("ready");
+
+function displayChangeOnReady() {
+  const curtain = document.getElementById("curtain");
+  const main = document.getElementById("board");
+  curtain.classList.add("closed");
+  main.style.display = "flex";
+}
+
+function setPageLink(link) {
+  const nextPage = document.getElementById("next");
+  nextPage.addEventListener("click", () => {
+    window.location.href = link;
+  });
+}
+
+function setNextPage(player) {
+  if (player === "player1") {
+    sessionStorage.setItem("setup", "player2");
+    setPageLink("shipSetup.html");
+  } else {
+    setPageLink("board.html");
+  }
+}
+
+readyBtn.addEventListener("click", () => {
+  displayChangeOnReady();
+  setNextPage(sessionStorage.getItem("setup"));
+});
 console.log("pagina de setup");
