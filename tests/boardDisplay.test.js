397ebdd8f5322898cbb33d8852752caa4d7f6ba0
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-26 11:34:40 -0500

    Agrega clases de css y typo en test

--- tests/boardDisplay.test.js
+++ tests/boardDisplay.test.js
@@ -1,6 +1,6 @@
 import * as gridDisplay from "../src/setupOfShip/boardDisplay";
 
-test.only("Ammount of cells made", () => {
+test("Ammount of cells made", () => {
   const appendChild = jest.fn((x) => true);
   const mockObj = { appendChild, test: true };
   gridDisplay.fillGrid(10, mockObj);
