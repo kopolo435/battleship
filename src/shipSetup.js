1483e437153ffcca19608eff908e1387ce54e993
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-26 10:04:13 -0500

    Implementa agregado de barcos, select de orientacion
    Se agrega un event listener para obtener la orientacion de los barcos
    Se agrega el codigo para agregar al gambeboard el nuevo

--- src/shipSetup.js
+++ src/shipSetup.js
@@ -1,20 +1,14 @@
 import style from "./shipSetup.css";
-import { fillGrid, addShipSelection } from "./setupOfShip/boardDisplay";
+import {
+  fillGrid,
+  addShipSelection,
+  displayChangeOnReady,
+  setPageLink,
+} from "./setupOfShip/boardDisplay";
+import Gameboard from "./gameboard";
 
-const readyBtn = document.getElementById("ready");
-const board = document.getElementById("board");
-
-function displayChangeOnReady() {
-  const curtain = document.getElementById("curtain");
-  curtain.classList.add("closed");
-}
-
-function setPageLink(link) {
-  const nextPage = document.getElementById("next");
-  nextPage.addEventListener("click", () => {
-    window.location.href = link;
-  });
-}
+const gameboard = new Gameboard();
+const ships = new Map();
 
 function setNextPage(player) {
   if (player === "player1") {
@@ -24,10 +18,50 @@ function setNextPage(player) {
     setPageLink("board.html");
   }
 }
+
+function createShip(coordinate) {
+  const length = sessionStorage.getItem("shipLength");
+  const orientation = sessionStorage.getItem("shipOrientation");
+  const result = gameboard.addShip(coordinate, length, orientation);
+  if (result) {
+    return gameboard.ships[gameboard.ships.length - 1];
+  }
+  return null;
+}
+
+function onCellClick(event) {
+  const coordinate = event.target.dataset.id;
+  if (sessionStorage.getItem("shipLength") !== null) {
+    const ship = createShip(coordinate);
+    if (ship) {
+      const shipId = sessionStorage.getItem("shipId");
+      ships.set(shipId, ship);
+    } else {
+      console.log("Elija otra posicion para el barco");
+    }
+  } else {
+    console.log("Debe escoger un barco primero");
+  }
+}
+
+const readyBtn = document.getElementById("ready");
+const board = document.getElementById("board");
+const orientation = document.getElementById("orientation");
 fillGrid(10, board);
-addShipSelection();
+const cellList = board.querySelectorAll(".cell");
+
+Array.from(cellList).forEach((cell) => {
+  cell.addEventListener("click", onCellClick);
+});
+
+orientation.addEventListener("change", () => {
+  sessionStorage.setItem("shipOrientation", orientation.value);
+});
+
 readyBtn.addEventListener("click", () => {
   displayChangeOnReady();
   setNextPage(sessionStorage.getItem("setup"));
 });
-console.log("pagina de setup");
+
+addShipSelection();
+sessionStorage.setItem("orientation", orientation.value);
