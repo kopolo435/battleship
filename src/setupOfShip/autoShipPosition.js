5e6ba96c1d03707d22bdd7e3dc21cb84871c6150
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-30 10:19:01 -0500

    Agrega comentarios documentando funciones

--- src/setupOfShip/autoShipPosition.js
+++ src/setupOfShip/autoShipPosition.js
@@ -1,6 +1,14 @@
+/* Modulo encargado de escoger las posiciones para los barcos de la computadora */
+
 import Gameboard from "../gameboard.js";
 import Ship from "../ship.js";
 
+/*
+Escoge una posicion aleatoria, de las posiciones validas que se le dan
+Parametros:
+map: type Map, coleecion de posiciones de un gameboard
+Retorno: string de una coordenada valida
+*/
 function selectCoordinates(map) {
   const validPositions = [];
   map.forEach((value, coordinate) => {
@@ -11,6 +19,12 @@ function selectCoordinates(map) {
   return validPositions[Math.floor(Math.random() * validPositions.length)];
 }
 
+/*
+Desordena aleatoriamente los elementos de un array
+Parametros:
+ships: type array, coleccion de ints
+Retorno: array desordenado
+*/
 function shuffle(ships) {
   const array = ships;
   let currentIndex = array.length;
@@ -30,7 +44,15 @@ function shuffle(ships) {
   return array;
 }
 
-function addShip(lenght, gameboard) {
+/* Obtiene una posicion valida en el gameboard indicado para la length especificada
+Parametros:
+lenght: string, pero se usa como un int
+gameboard: Gameboard, se usa para validar las posiciones posibles
+Retorno: retorna un objeto con la informacion de una posicion valida
+si retorna null, hay un error grave
+*/
+
+function getValidPosition(lenght, gameboard) {
   const stop = false;
   while (!stop) {
     const coordinate = selectCoordinates(gameboard.getCells());
@@ -43,13 +65,20 @@ function addShip(lenght, gameboard) {
       return { coordinate, orientation: "horizontal", lenght };
     }
   }
+  return null;
 }
 
+/* Crea un Gameboard object con posiciones validas
+Parametros:
+ships: Array con coleccion de ints, o string, pero son numeros (1 o ",2")
+cada uno indica la length de un barco
+Retorno: Gameboard object con barcos colocados en posiciones validas
+ */
 function createGameboard(ships) {
   const gameboard = new Gameboard();
   const shuffledShips = shuffle(ships);
   shuffledShips.forEach((length) => {
-    const shipData = addShip(length, gameboard);
+    const shipData = getValidPosition(length, gameboard);
     gameboard.addShip(
       shipData.coordinate,
       shipData.lenght,
@@ -58,6 +87,11 @@ function createGameboard(ships) {
   });
   return gameboard;
 }
+
+/* Toma una nodeList de botones y crea un gameboard con posiciones
+validas de las length especificada por los botones
+Parametros: buttonsNode, nodeList de elementos button
+Retorna: Gameboard object con posiciones validas de ships */
 
 function computerPositions(buttonsNode) {
   const shipsLengths = [];
