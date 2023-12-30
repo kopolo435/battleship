f2706e0dd3b9054ebe69d5882610e638c87e6495
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-30 13:16:49 -0500

    Documenta index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import style from "./styles/index.css";
 
-const startLink = document.getElementById("start");
+const startBtn = document.getElementById("start");
 
+/**
+ *
+ * @param {string} player indica de que player se guardan los datos
+ * player1 | player2
+ */
 function setPlayersInfo(player) {
   let name = document.getElementById(`${player}`).value;
-  name = name === "" ? player : name;
+  name = name === "" ? player : name; // Comprueba si colocar nombre default
   const isComputer = document.getElementById(`${player}Ia`).checked;
 
   const playerObj = { name, isComputer };
   sessionStorage.setItem(player, JSON.stringify(playerObj));
 }
-startLink.addEventListener("click", (e) => {
+startBtn.addEventListener("click", (e) => {
   e.preventDefault();
   setPlayersInfo("player1");
   setPlayersInfo("player2");
