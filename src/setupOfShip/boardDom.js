ab2560c8ba331f2e438e52039da11796c83a2bd6
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-30 12:06:33 -0500

    Documenta con comentarios modulo de DOM de board.html

--- src/setupOfShip/boardDom.js
+++ src/setupOfShip/boardDom.js
@@ -1,15 +1,29 @@
+/* Modulo que se encarga de modifica el DOM del board.html */
+
 const typingEffect = null; // Global variable to hold the interval
 
+/**
+ * Remueve la clase que oculta el curtain, que bloquee la vista del board
+ */
 function showCurtain() {
   const curtain = document.getElementById("curtain");
   curtain.classList.remove("closed");
 }
 
+/**
+ * Agrega la clase que muestra el curtain para que bloquee la vista del board
+ */
 function hideCurtain() {
   const curtain = document.getElementById("curtain");
   curtain.classList.add("closed");
 }
 
+/**
+ * Crea div element con una clase dependiendo del type pasado
+ * @param {string} coordinate formato "[2,3]"
+ * @param {string} type formtato "ship","empty","hit","miss"
+ * @returns
+ */
 function createCell(coordinate, type) {
   const div = document.createElement("div");
   div.classList.add("cell");
@@ -32,6 +46,11 @@ function createCell(coordinate, type) {
   return div;
 }
 
+/**
+ * Agrega clases dependiendo del tipo de celda en el mapa enemigo
+ * @param {divElement} cell
+ * @param {Map} enemyMap formato "[2,4]"=> "hit" | "miss" | "empty" | "ship"
+ */
 function hoverEvent(cell, enemyMap) {
   if (
     enemyMap.get(cell.dataset.id) !== "hit" &&
@@ -44,6 +63,10 @@ function hoverEvent(cell, enemyMap) {
   }
 }
 
+/**
+ * Se encarga de agregar eventos de hover a cada cell
+ * @param {Map} enemyMap formato "[2,4]"=> "hit" | "miss" | "empty" | "ship"
+ */
 function addCellsHoverEvent(enemyMap) {
   const enemyBoard = document.getElementById("enemyBoard");
   const cells = enemyBoard.querySelectorAll(".cell");
@@ -57,7 +80,12 @@ function addCellsHoverEvent(enemyMap) {
   });
 }
 
-function fillBoard(gameboard, showShips) {
+/**
+ * Llena el board mostrando o no las celdas con barcos en base al parametro
+ * @param {Map} gameboardCells formato "[2,4]"=> "hit" | "miss" | "empty" | "ship"
+ * @param {boolean} showShips
+ */
+function fillBoard(gameboardCells, showShips) {
   if (showShips) {
     const currentBoard = document.getElementById("currentBoard");
     currentBoard.replaceChildren();
@@ -65,7 +93,7 @@ function fillBoard(gameboard, showShips) {
       for (let x = 0; x < 10; x += 1) {
         const coordinate = `[${x},${y}]`;
         currentBoard.appendChild(
-          createCell(coordinate, gameboard.get(coordinate))
+          createCell(coordinate, gameboardCells.get(coordinate))
         );
       }
     }
@@ -86,14 +114,22 @@ function fillBoard(gameboard, showShips) {
   }
 }
 
-function finalBoard(gameboard, isWinner) {
+/**
+ * Llena los tableros al finalizar el juego, colocando el tablero del ganador en el board
+ * de currentBoard
+ * @param {Map} gameboardCells formato "[2,4]"=> "hit" | "miss" | "empty" | "ship"
+ * @param {boolean} isWinner
+ */
+function finalBoard(gameboardCells, isWinner) {
   if (isWinner) {
     const board = document.getElementById("currentBoard");
     board.replaceChildren();
     for (let y = 10 - 1; y >= 0; y -= 1) {
       for (let x = 0; x < 10; x += 1) {
         const coordinate = `[${x},${y}]`;
-        board.appendChild(createCell(coordinate, gameboard.get(coordinate)));
+        board.appendChild(
+          createCell(coordinate, gameboardCells.get(coordinate))
+        );
       }
     }
   } else {
@@ -102,12 +138,18 @@ function finalBoard(gameboard, isWinner) {
     for (let y = 10 - 1; y >= 0; y -= 1) {
       for (let x = 0; x < 10; x += 1) {
         const coordinate = `[${x},${y}]`;
-        board.appendChild(createCell(coordinate, gameboard.get(coordinate)));
+        board.appendChild(
+          createCell(coordinate, gameboardCells.get(coordinate))
+        );
       }
     }
   }
 }
 
+/**
+ * Coloca el nombre del jugador especificado en el curtain
+ * @param {string} name
+ */
 function setCurtainName(name) {
   const curtainName = document.getElementById("playerName");
   curtainName.textContent = name;
